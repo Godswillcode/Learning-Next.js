6b3b16fa8eaee35dede051ac702c04a73559a4f0
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-05-16 13:26:49 +0100

    Update index.js

--- pre-rendering/pages/news/index.js
+++ pre-rendering/pages/news/index.js
@@ -1,6 +1,27 @@
-function NewsArticleList(params) {
-  return <h2>List of news</h2>;
+function NewsArticleList({ articles }) {
+  return (
+    <>
+      <h2>List of news</h2>
+      {articles.map((article) => (
+        <div key={article.id}>
+          <h3>
+            {article.id} {article.title} | {article.category}
+          </h3>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default NewsArticleList;
 
+export async function getServerSideProps() {
+  const data = await fetch("http://localhost:4000/news");
+  const jsonData = await data.json();
+
+  return {
+    props: {
+      articles: jsonData,
+    },
+  };
+}
