820d90237be2a498074ae91c4fee485ba738f1c0
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-05-26 15:56:54 +0100

    finished todo

---
+++ todo-app/src/components/TodoItem.tsx
@@ -0,0 +1,33 @@
+"use client" 
+
+type todoItemProps = {
+  id: string;
+  title: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
+};
+
+export const TodoItem = ({
+  complete,
+  id,
+  title,
+  toggleTodo,
+}: todoItemProps) => {
+  return (
+    <li className="flex gap-1 items-center">
+      <input
+        id={id}
+        type="checkbox"
+        className="cursor-pointer peer"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)} 
+      />
+      <label
+        htmlFor={id}
+        className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500"
+      >
+        {title}
+      </label>
+    </li>
+  );
+};
